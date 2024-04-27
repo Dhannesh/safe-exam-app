37d40ed9b8dbf8b27144a48ece8cc08bd3511400
Dhaneshwar Kumar <dhannesh@gmail.com>
2024-04-27 10:38:02 +0530

    Update link for ACPC

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,1 +1,1 @@
-export const testLink = "https://www.hackerrank.com/coding-test-b-1242024";
+export const testLink = "https://codeforces.com/contestInvitation/f27ad73f6b64080de42373ee7a2cff179e9960d6";
